6fca0b8b277bc38c6988e92f6ca0239373b77a11
filipematiasp <filipematiasp@gmail.com>
2023-11-11 00:04:28 -0300

    Layout building

---
+++ desafio_ahgora/src/components/SearchSequenceBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container min-vh-100 d-flex flex-column justify-content-center align-items-center">
+    <div>
+      <p class="h3">Entre com a sequência de DNA</p>
+    </div>
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Entre com a sequência: EX:['CTGAGA', 'CTATGC', 'TATTGT', 'AGAGGG', 'CCCCTA', 'TCACTG']" aria-label="Sequence dna" aria-describedby="button-addon2">
+      <button class="btn btn-outline-secondary" type="button" id="button-addon2">Analisar</button>
+    </div>
+    <div>
+      <p class="h3">{{species}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchSequenceBar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
